015f9b7091f0fdd89bbd468237e285a7987b778e
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-12-08 10:56:01 -0300

    ConfigurationServerApplication

--- src/main/java/com/example/demo/ConfigurationServerApplication.java
+++ src/main/java/com/example/demo/ConfigurationServerApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class MicroConfigserverApplication {
+@EnableConfigServer
+public class ConfigurationServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroConfigserverApplication.class, args);
+		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
 
 }
